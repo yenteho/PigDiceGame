b4cd6c7ccb5a6d1b049d4280b39809498b67509f
YenTe Ho <ythodesign@gmail.com>
2018-05-26 15:46:58 +0800

    Updating Scores and Changing the Active Player

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ GAME RULES:
 var scores, roundScore, activePlayer;
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = '0';
@@ -27,5 +27,21 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
     // 3. Update the round score IF the rolled number was not a "1"
+    if (dice !== 1){
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
